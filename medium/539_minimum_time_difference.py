35e45d2949f73074a1c79c65b7655a97edbc6178
Sharath C <sharathc77777@gmail.com>
2026-09-09 18:54:34 +0530

    leetcode 539 completed

---
+++ medium/539_minimum_time_difference.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        times = []
+
+        for time in timePoints:
+            hour, minute = map(int, time.split(":"))
+            times.append(hour * 60 + minute)
+
+        times.sort()
+
+        answer = float('inf')
+
+        for i in range(1, len(times)):
+            answer = min(answer, times[i] - times[i - 1])
+
+        # Difference between last time and first time across midnight
+        answer = min(answer, 1440 - times[-1] + times[0])
+
+        return answer
