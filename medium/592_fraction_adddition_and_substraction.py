e2fdffe14c86013461dc3b74044e69d67a3bbfa5
Sharath C <sharathc77777@gmail.com>
2026-09-09 18:56:21 +0530

    leetcode 592 completed

---
+++ medium/592_fraction_adddition_and_substraction.py
@@ -0,0 +1,35 @@
+from fractions import Fraction
+
+class Solution:
+    def fractionAddition(self, expression: str) -> str:
+        result = Fraction(0, 1)
+
+        i = 0
+
+        while i < len(expression):
+            sign = 1
+
+            if expression[i] == '+':
+                i += 1
+            elif expression[i] == '-':
+                sign = -1
+                i += 1
+
+            j = expression.find('/', i)
+
+            numerator = int(expression[i:j])
+
+            i = j + 1
+
+            j = i
+
+            while j < len(expression) and expression[j].isdigit():
+                j += 1
+
+            denominator = int(expression[i:j])
+
+            result += Fraction(sign * numerator, denominator)
+
+            i = j
+
+        return f"{result.numerator}/{result.denominator}"
