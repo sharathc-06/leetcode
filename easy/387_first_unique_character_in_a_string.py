9278048549ecc6f5b69fffdb80163f859827c80a
Sharath C <sharathc77777@gmail.com>
2026-09-11 22:50:37 +0530

    leetcode 387 completed

---
+++ easy/387_first_unique_character_in_a_string.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count = Counter(s)
+
+        for i in range(len(s)):
+            if count[s[i]] == 1:
+                return i
+
+        return -1
